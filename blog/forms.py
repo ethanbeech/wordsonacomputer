305399d4c983c7c136ae14b984b632eecd3cfc8d
ethanbeech <66847782+ethanbeech@users.noreply.github.com>
2020-07-08 15:18:56 +0100

    Styling for comments

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from .models import Comment
-
+from crispy_forms.helper import FormHelper
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.fields["body"].label = False
